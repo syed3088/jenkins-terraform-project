3958ccc294c4a2074faa8a07c8957e33d6838b44
Syed Khadar <69810725+syed3088@users.noreply.github.com>
2024-11-30 16:20:29 +0530

    Update main.tf

--- Tf course/modules/ec2/main.tf
+++ Tf course/modules/ec2/main.tf
@@ -46,7 +46,7 @@ resource "aws_instance" "example" {
      inline = [
     "sudo apt update -y",                           # Update the server
     "sudo apt install -y apache2",                   # Install Apache HTTP server (Ubuntu package)
-    "echo '<html><body><h1>Hello, World from Terraform!</h1></body></html>' | sudo tee /var/www/html/index.html",  # Create a simple HTML page
+    #"echo '<html><body><h1>Hello, World from Terraform!</h1></body></html>' | sudo tee /var/www/html/index.html",  # Create a simple HTML page
     "sudo systemctl start apache2",                  # Start Apache HTTP server
     "sudo systemctl enable apache2",                 # Enable Apache HTTP server to start on boot
     "sudo ufw allow 'Apache'  # Allow HTTP traffic through firewall"  # Open HTTP port in firewall (if UFW is enabled)
